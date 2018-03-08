c0a85cebc6117ce61b6cc3ed0c93d88a9da99f0b
Hwi Yong Song <hysong15@gmail.com>
2018-03-08 13:43:30 -0800

    Initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+
+
+let components: Array<any> = [
+  MyApp,
+  HomePage
+]
+
+let providers: Array<any> = [
+  SplashScreen,
+  StatusBar,
+  {provide: ErrorHandler, useClass: IonicErrorHandler}
+]
+
+let directives: Array<any> = [
+
+]
+
+let pipes: Array<any> = [
+
+]
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [...components, ...directives, ...pipes],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  entryComponents: components,
+  providers: providers,
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class AppModule {}
